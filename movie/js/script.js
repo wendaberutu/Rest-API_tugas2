085ded1675a529d68e26088e27706dd30f6bd290
Wenda Berutu <wendaberutu22@example.com>
2024-09-16 20:09:42 +0700

    membuat ig dan yt

--- movie/js/script.js
+++ movie/js/script.js
@@ -1,51 +1,86 @@
-function searchMovie(){
+function searchMovie() {
     $('#movie-list').html('');
 
     $.ajax({
-        url:'http://omdbapi.com',
-        type:'get',
-        dataType :'json',
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
         data: {
-            'apikey' :'cee70945',
-            's' : $('#search-input').val()
+            'apikey': 'cee70945',
+            's': $('#search-input').val()
         },
-        success : function(result){
-            if(result.Response =="True"){
+        success: function (result) {
+            if (result.Response == "True") {
                 let movies = result.Search;
 
-                $.each(movies, function(i,data){
+                $.each(movies, function (i, data) {
                     $('#movie-list').append(`
                         <div class="col-md-4">
-                       <div class="card" style="width: 18rem;">
-                        <img src="`+data.Poster+`" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">`+data.Title+`</h5>
-                           <h6 class="card-subtitle mb-2 text-body-secondary">`+data.Year+`</h6>
-                         <a href="#" class="card-link" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail</a>
-                          </div>
-                            </div> 
-                            </div>`
+                            <div class="card" style="width: 18rem;">
+                                <img src="`+ data.Poster + `" class="card-img-top" alt="Poster">
+                                <div class="card-body">
+                                    <h5 class="card-title">`+ data.Title + `</h5>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year + `</h6>
+                                    <a href="#" class="card-link see-detail" data-id="`+ data.imdbID + `" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail</a>
+                                </div>
+                            </div>
+                        </div>`
                     );
                 });
 
                 $('#search-input').val('');
 
-            }else {
+            } else {
                 $('#movie-list').html(`
-                    <div class="col>
-                    <h1 class ="text-center">`+ result.Error+ `<h1>
+                    <div class="col">
+                        <h1 class="text-center">`+ result.Error + `</h1>
                     </div>
-                    `)
+                `);
             }
-
         }
     });
 }
-$('#search-button').on('click',function(){
-   searchMovie();
+
+$('#search-button').on('click', function () {
+    searchMovie();
 });
-$('#search-input').on('keyup',function(e){
-    if(e.keyCode ===13){
+
+$('#search-input').on('keyup', function (e) {
+    if (e.keyCode === 13) {
         searchMovie();
     }
+});
+
+$('#movie-list').on('click', '.see-detail', function () {
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'cee70945',
+            'i': $(this).data('id')
+        },
+        success: function (movie) {
+            if (movie.Response === "True") {
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="`+ movie.Poster + `" class="img-fluid" alt="Poster">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>`+ movie.Title + `</h3></li>
+                                    <li class="list-group-item">Released: `+ movie.Released + `</li>
+                                    <li class="list-group-item">Genre: `+ movie.Genre + `</li>
+                                    <li class="list-group-item">Director: `+ movie.Director + `</li>
+                                    <li class="list-group-item">Plot: `+ movie.Plot + `</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`
+                );
+            }
+        }
+    });
 });
